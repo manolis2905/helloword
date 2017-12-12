d9951a76500813ec9d01f005f1ffc958bfe82ccb
Manolis <manolisoil13@gmail.com>
2017-12-12 02:33:43 +0200

    create new class

--- helloword/src/gr/dit/hua/helloword/Person.java
+++ helloword/src/gr/dit/hua/helloword/Person.java
@@ -5,6 +5,13 @@ public class Person {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello word");
+		Person1 x;
+		Person1 y;
+		x=new Person1("Manolis","Soiles", 25);
+		y=new Person1("Florios","Soiles", 30);
+		System.out.println("My name is " + x.getFirstname() + ".");
+		y.setFirstname("Giannis");
+		System.out.println("My name is " + y.getFirstname() + ".");
 	}
 
 }
